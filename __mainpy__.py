6d47dc8d2fcaa4d14672c1fe435ac07e51ae0887
agd30 <agd30072010@gmail.com>
2021-08-24 16:47:11 +0300

    голл голл голл МНОГО ГОЛОВ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- __mainpy__.py
+++ __mainpy__.py
@@ -12,7 +12,7 @@ while True:
     screen.fill([100, 100, 100])
     if model.ball!=None:
         model.ball.draw(screen)
-    model.ball2.draw(screen)
+    # model.ball2.draw(screen)
     model.vorota_left.draw(screen)
     model.vorota_right.draw(screen)
     model.igroc_left.draw(screen)
--- model.py
+++ model.py
@@ -1,25 +1,26 @@
-import vorota, ball as modul_ball, igroc
+import vorota, ball as modul_ball, igroc, random, controller
 
 vorota_right = vorota.Vorota(1120, 0)
 vorota_left = vorota.Vorota(0, 0)
-ball = modul_ball.Ball(575, 300,30,5,-5)
-ball2=modul_ball.Ball(575,300,30,10,10)
+ball = modul_ball.Ball(575, 300, 30, 5, -5)
+# ball2=modul_ball.Ball(575,300,30,10,10)
 igroc_left = igroc.Igroc(200, 233)
 igroc_right = igroc.Igroc(920, 233)
+
+
+def make_ball():
+    global ball
+    ball = modul_ball.Ball(575, 300, 30, random.choice([5, -5]), -5)
+
+
 def step():
     global ball
-    if ball!=None:
-        a=ball.goal(vorota_left,vorota_right)
-        ball.dvigenie(igroc_left,igroc_right)
-        if a==True:
-            ball=None
-    ball2.dvigenie(igroc_left, igroc_right)
 
+    if ball != None:
+        a = ball.goal(vorota_left, vorota_right)
+        ball.dvigenie(igroc_left, igroc_right)
+        if a == True:
+            ball = None
+            controller.start_timers()
 
-
-
-
-
-
-
-
+    # ball2.dvigenie(igroc_left, igroc_right)
