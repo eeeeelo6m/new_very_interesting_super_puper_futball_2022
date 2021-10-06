cb6d74edd1a71e0aba1405a753c42b5c723d3856
agd30 <agd30072010@gmail.com>
2021-10-06 17:05:36 +0300

    крипченьки

--- MONEEEEEEEEEEEEEEEEEEEEEEEEEEEESSSSSSS.py
+++ MONEEEEEEEEEEEEEEEEEEEEEEEEEEEESSSSSSS.py
@@ -5,6 +5,13 @@ def make_moneeesss():
     global mone
     moneeeeeeessss.append(pygame.Rect(random.choice([200,920]),random.randint(0,590),30,30))
     mone+=1
-
+a={'name':'Andrey', 'age':12}
+b={'name':'Vova','age':1}
+c=[a,b]
+a['age']=13
+del b
+for b in c:
+    b['age']+=1
+    b['year']=2021- b['age']
 
 
